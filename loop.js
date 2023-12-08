566fb7d1bad2e0af3bb6d60b7fa5147c470181d4
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-08 23:40:52 +0600

    Event loop

--- loop.js
+++ loop.js
@@ -1,6 +1,14 @@
-// node loop.js
+// node myFile.js
+myFile.runContents();
 
+function shuldContinue() {
 
+}
+
+// Entire body executes in one 'tick'
+while(shouldContinue()) {
+
+}
 
 
 // exist back to terminal

7b3d10e828fca65cfa92ea73d9fc8835a92ca0a7
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-09 01:10:42 +0600

    Single Thread

--- threads.js
+++ threads.js
@@ -4,3 +4,7 @@ const start = Date.now();
 crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
     console.log('1:', Date.now() - start);
 });
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('2:', Date.now() - start);
+});

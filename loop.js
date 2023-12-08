94c0f0779ddede215a7cdd8ce314ee6701052bae
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-08 23:44:08 +0600

    Event loop

--- loop.js
+++ loop.js
@@ -2,7 +2,9 @@
 myFile.runContents();
 
 function shuldContinue() {
-
+    // Check one: Any pending setTimeout, setInterval, setImmediate?
+    // Checkout two: Any pending OS tasks? (Like server listening to port)
+    // Check three: Any pending long running operations? (Like fs module)
 }
 
 // Entire body executes in one 'tick'

13787e9e1c833f0f1c6b7669d4f2c836a6db15a2
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-08 23:48:51 +0600

    Event loop

--- loop.js
+++ loop.js
@@ -1,10 +1,17 @@
 // node myFile.js
+
+const pendingTimers = [];
+const pendingOSTasks = [];
+const pendingOperations = [];
+
+// New timers, tasks, operations are recorded from myFile ruuning
 myFile.runContents();
 
 function shuldContinue() {
     // Check one: Any pending setTimeout, setInterval, setImmediate?
     // Checkout two: Any pending OS tasks? (Like server listening to port)
     // Check three: Any pending long running operations? (Like fs module)
+    return pendingTimers.length || pendingOSTasks.length || pendingOperations.length;
 }
 
 // Entire body executes in one 'tick'

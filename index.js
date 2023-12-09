59e45f581b2695c527e10f83bcd878206d1f1016
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-09 22:19:10 +0600

    Cluster

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-function doWork(duration) {
-    const start = Date.now();
-    while(Date.now() - start < duration) {}
+if(cluster.isMaster) {
+    cluster.fork();
+
+} else {
+    const express = require('express');
+    const app = express();
+
+    function doWork(duration) {
+        const start = Date.now();
+        while(Date.now() - start < duration) {}
+    }
+
+    app.get('/', (req, res) => {
+        doWork(5000);
+        res.send('Hi there');
+    });
+
+    app.listen(3000);
 }
-
-app.get('/', (req, res) => {
-    doWork(5000);
-    res.send('Hi there');
-});
-
-app.listen(3000);

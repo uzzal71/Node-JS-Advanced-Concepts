5c7d00b72296a2646be436d35ecaa92dff99580b
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-09 22:28:04 +0600

    Cluster

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const cluster = require('cluster');
 
+// Is the file being executed in master node?
 if(cluster.isMaster) {
+    // Cause index.js to be executed *again` but
+    // in child mode
     cluster.fork();
-
+    // cluster.fork();
+    // cluster.fork();
+    // cluster.fork();
 } else {
+    // I'm a child, i'm going to act like a server
+    // and do nothing else
     const express = require('express');
     const app = express();
 
@@ -16,6 +23,10 @@ if(cluster.isMaster) {
         doWork(5000);
         res.send('Hi there');
     });
+
+    app.get('/fast', (req, res) => {
+        res.send('This was fast!');
+    })
 
     app.listen(3000);
 }

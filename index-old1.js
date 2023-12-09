824689af0b01402f7fcc17f9ad4aa8c7578c778f
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-09 22:59:50 +0600

    Cluster

--- index-old1.js
+++ index-old1.js
@@ -1,4 +1,3 @@
-process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require('cluster');
 
 // Is the file being executed in master node?
@@ -10,14 +9,16 @@ if(cluster.isMaster) {
     // I'm a child, i'm going to act like a server
     // and do nothing else
     const express = require('express');
-    const crypto = require('crypto');
     const app = express();
 
+    function doWork(duration) {
+        const start = Date.now();
+        while(Date.now() - start < duration) {}
+    }
 
     app.get('/', (req, res) => {
-        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-            res.send('Hi there');
-        });
+        doWork(5000);
+        res.send('Hi there');
     });
 
     app.get('/fast', (req, res) => {

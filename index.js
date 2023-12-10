1428e7fa943bb87e0ec080676e4499cb5241e55b
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-10 23:23:23 +0600

    Worker Thread

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-        res.send('Hi there');
-    });
+    
 });
 
 app.get('/fast', (req, res) => {

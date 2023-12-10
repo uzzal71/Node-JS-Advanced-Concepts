061b7ec01c456d1c37005b676f0353a73e61dd38
uzzal71 <uzzalroy.acm@gmail.com>
2023-12-10 23:47:32 +0600

    Worker Thread

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-// I'm a child, i'm going to act like a server
-// and do nothing else
 const express = require('express');
-const crypto = require('crypto');
-const Worker = require('webworker-threads').Worker;
+const { Worker, isMainThread } = require('worker_threads');
 const app = express();
 
+if (isMainThread) {
+  app.get('/', (req, res) => {
+    const worker = new Worker(__filename);
 
-app.get('/', (req, res) => {
-    const worker = new Worker(function() {
-        this.onmessage = function() {
-            let counter = 0;
-            while(counter < 1e9) { // 10000000000000
-                counter++;
-            }
-
-            postMessage(counter);
-        }
+    worker.on('message', (myCounter) => {
+      console.log(myCounter);
+      res.send(`Counter: ${myCounter}`);
     });
 
-    worker.onmessage = function(myCounter) {
-        console.log(myCounter);
-    }
+    worker.postMessage('start');
+  });
 
-    worker.postMessage();
-});
-
-app.get('/fast', (req, res) => {
+  app.get('/fast', (req, res) => {
     res.send('This was fast!');
-})
+  });
 
-app.listen(3000);
+  app.listen(3000);
+} else {
+  let counter = 0;
+  while (counter < 1e9) {
+    counter++;
+  }
+
+  parentPort.postMessage(counter);
+}
